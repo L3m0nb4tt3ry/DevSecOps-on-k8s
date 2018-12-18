559c4eae8ff28c6274c2800044d1be72ffb2f2d6
Onkar Dhane <engg.onkar@gmail.com>
2018-12-18 13:43:57 +0530

    Created startup script
    Clubbed initialization commands

---
+++ startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd DevSecOps-on-k8s
+ansible-playbook -i hosts ~/kube-cluster/initial.yml
+ansible-playbook -i hosts ~/kube-cluster/kube-dependencies.yml
+ansible-playbook -i hosts ~/kube-cluster/master.yml
